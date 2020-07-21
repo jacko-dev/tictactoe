dc0065260e4d1f3ee42cb965df80bbfd6d28f9e3
toadio <jackson.m.philips@gmail.com>
2020-07-21 00:18:41 +0000

    Made a reset button, starting to make functionality

---
+++ script.js
@@ -0,0 +1,23 @@
+button1 = document.getElementById("one");
+button2 = document.getElementById("two");
+button3 = document.getElementById("three");
+button4 = document.getElementById("four");
+button5 = document.getElementById("five");
+button6 = document.getElementById("six");
+button7 = document.getElementById("seven");
+button8 = document.getElementById("eight");
+button9 = document.getElementById("nine");
+sqcap1 = "Empty";
+sqcap2 = "Empty";
+sqcap3 = "Empty";
+sqcap4 = "Empty";
+sqcap5 = "Empty";
+sqcap6 = "Empty";
+sqcap7 = "Empty";
+sqcap8 = "Empty";
+sqcap9 = "Empty";
+button1.addEventListener('click', () => {
+  if (sqcap1 == "Empty") {
+    document.getElementById("")
+  }
+});
